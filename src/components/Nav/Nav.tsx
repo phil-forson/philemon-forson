33a402a3d922b956eee6019332f0d877d7bba02a
Philemon Forson <firstkoolaid21@gmail.com>
2024-11-14 22:30:31 +0000

    fixed nav for medium sized devices

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,15 +4,15 @@ import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 const Nav = () => {
   return (
     <header
-      className="header_main sticky top-0 flex justify-between items-center "
+      className="header_main sticky top-0 flex justify-between items-center"
     >
       <div className=" gap-2 items-center hidden md:flex">
-        <p className="text-lg ">Philemon Forson</p>
+        <p className="text-sm">Philemon Forson</p>
         <p className="px-2 py-1 text-xs bg-[hsl(var(--muted))] rounded-md">
           Software Developer And Data Engineer
         </p>
       </div>
-      <div className="flex space-x-5 justify-around  items-center text-sm w-full">
+      <div className="flex space-x-5 justify-around md:justify-start items-center text-sm w-full md:w-auto">
       <div className="">Home</div>
         <div className="">About</div>
         <div className="">Projects</div>
