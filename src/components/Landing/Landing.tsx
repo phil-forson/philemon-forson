b449853a6bf3aebc56a106b34d07f78f0c9c5c4c
Philemon Forson <firstkoolaid21@gmail.com>
2024-11-14 22:44:49 +0000

    edited view height for landing

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -3,7 +3,7 @@ import { BiCross } from "react-icons/bi";
 
 const Landing = () => {
   return (
-    <section className="w-full h-[calc(100vh-64px)] md:h-screen md:px-36 px-4 md:py-16 relative">
+    <section className="w-full h-[80vh] md:h-screen md:px-36 px-4 md:py-16 relative">
       <div className="md:relative h-full md:h-auto ">
         <div className="crosshair relative hidden md:block"></div>
         <div className="md:border border-[hsla(var(--ds-gray-200-value),1)] h-full md:h-[30rem] md:w-[calc(100vw-21rem)] md:absolute md:top-[10px] md:left-[10px] md:px-28 flex flex-col justify-center md:gap-7 gap-4 items-center ">
